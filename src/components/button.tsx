ab2c298ebf510189634ce6107da649ebb2219d73
Wellington Barbosa <huehueb58@gmail.com>
2026-05-19 22:05:04 -0300

    refactor: update Button component to spread rest props instead of using onClick directly

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,6 +2,6 @@ type Props = React.ComponentProps<"button"> & {
   name: string;
 };
 
-export function Button({ name, onClick }: Props) {
-  return <button onClick={onClick}>{name}</button>;
+export function Button({ name, ...rest }: Props) {
+  return <button {...rest}>{name}</button>;
 }
