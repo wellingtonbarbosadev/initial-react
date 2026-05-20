02fc5a78dca02857eae61a054d1fa9a9040d8633
Wellington Barbosa <huehueb58@gmail.com>
2026-05-19 21:55:35 -0300

    refactor: destructure props in Button component for cleaner code

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,6 +2,6 @@ type Props = {
   name: string;
 };
 
-export function Button(props: Props) {
-  return <button>{props.name}</button>;
+export function Button({ name }: Props) {
+  return <button>{name}</button>;
 }
